ce92d81f101c75cb97b89cbf6e8e117ea85488fc
anthonymang <anthonymanganielloda@gmail.com>
2021-06-07 11:51:04 -0400

    cusers: create login route/function

--- api/users.js
+++ api/users.js
@@ -49,16 +49,66 @@ const signup = async (req, res) => {
 
 }
 
+const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.findOne({email})
+        console.log(user)
+
+        if (!user) {
+            return res.status(400).json({message: 'User not found....'})
+        } else {
+            let isMatch = await bcrypt.compare(password, user.password);
+            console.log('password correct', isMatch)
+            if (isMatch){
+                // add one to timesLoggedIn
+                let logs = user.timesLoggedIn + 1
+                user.timesLoggedIn = logs;
+                const savedUser = await user.save();
+
+                const payload = {
+                    id: user.id,
+                    email: user.email,
+                    name: user.name,
+                    expiredToken: Date.now()
+                }
+
+                try {
+                    let token = await jwt.sign(payload, JWT_SECRET, {expiresIn: 3600})
+                    console.log('token', token)
+                    let legit = await jwt.verify(token, JWT_SECRET, {expiresIn: 60})
+                    res.json({
+                        success: true,
+                        token: `Bearer ${token}`,
+                        userData: legit
+                    })
+                } catch (error) {
+                    console.log('----Error in isMatch conditional----')
+                    console.log(error)
+                    res.status(400).json({message: 'Session has ended. Please log in again.'})
+                }
+            } else {
+                res.status(400).json({message: 'Either email or password is incorrect'})
+            }
+        }
+    } catch (error) {
+        console.log(`--- Error inside of /api/users/login`)
+        console.log(error)
+        return res.status(400).json({message: 'Either email or password is incorrect'})
+    }
+   
+}
+
 // routes
 router.get('/test', test);
 
+// POST api/users/register (Public)
 router.post('/signup', signup)
 
-// POST api/users/register (Public)
-// router.post('/signup', signup);
 
 // POST api/users/login (Public)
-// router.post('/login', login);
+router.post('/login', login);
 
 // GET api/users/current (Private)
 // router.get('/profile', passport.authenticate('jwt', { session: false }), profile);
