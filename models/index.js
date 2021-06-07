e21708a2feeded324facd50b74ab36f5d56af5f8
anthonymang <anthonymanganielloda@gmail.com>
2021-06-07 10:26:16 -0400

    add schema, connect to database

--- models/index.js
+++ models/index.js
@@ -1,1 +1,26 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+});
+
+const db = mongoose.connection;
+
+// Set up event for db to print connection
+db.once('open', () => {
+    console.log(`Connect to MongoDB at ${db.host}: ${db.port}`)
+})
+
+db.on('error', (error) =>{
+    console.log(`Database error`, error)
+})
+
+// import all models here
+const User = require('./User')
+
+module.exports = {
+    User
+}
